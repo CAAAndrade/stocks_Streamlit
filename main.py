b504687c115f54d8ed6e0d8b42eac16b0756080f
CAAAndrade <159110347+CAAAndrade@users.noreply.github.com>
2024-11-04 06:03:38 +0000

    adding NASDAQ

--- main.py
+++ main.py
@@ -12,27 +12,41 @@ from datetime import timedelta, datetime
 
 @st.cache_data  # armazena os dados gerados em carregar_dados no cache
 def carregar_dados(empresas):
-    
     texto_tickers = " ".join(empresas)
     dados_acao = yf.Tickers(texto_tickers)
-    cotacoes_acao = dados_acao.history(period="1d", start="2020-1-1", end=datetime.now())
+    cotacoes_acao = dados_acao.history(
+        period="1d", start="2020-1-1", end=datetime.now()
+    )
     cotacoes_acao = cotacoes_acao["Close"]
     return cotacoes_acao
 
+
 @st.cache_data
-def carregar_tickers_acoes():
-    base_tickers = pd.read_csv("IBOV.csv", sep=",")
-    tickers = list(base_tickers["Codigo"])
-    tickers = [item.rstrip(" ") + ".SA" for item in tickers]
+def get_stocks_tickets(market):
+    base_tickers = pd.read_csv(f"{market}.csv", sep=",",header=0)
+    if market == "IBOV":
+        tickers = base_tickers.iloc[:, 0].tolist()
+        tickers = [item.rstrip(" ") + ".SA" for item in tickers]
+    else:
+        tickers = base_tickers.iloc[:, 0].tolist()
     return tickers
 
 
-
-acoes = carregar_tickers_acoes()
-dados = carregar_dados(acoes)
+def get_stocks_name(market):
+    base_tickers = pd.read_csv(f"{market}.csv", sep=",",header=0)
+    company_name = base_tickers.iloc[:, 1].tolist()
+    return company_name
 
 # prepara as visualizacoes - filtros
 st.sidebar.header("Filtros:")
+
+market = st.sidebar.selectbox("Selecione o mercado:", ("IBOV", "NASDAQ"))
+
+acoes = get_stocks_tickets(market)
+dados = carregar_dados(acoes)
+name = get_stocks_name(market)
+
+
 
 lista_acoes = st.sidebar.multiselect("Selecione as ações para sua carteira", dados.columns)
 if lista_acoes:
@@ -55,57 +69,60 @@ st.write("""
          """)  # markdown
 
 # criar graficos
-st.line_chart(dados)
-
-# calculo de performance
-
-texto_performance = ""
 if len(lista_acoes) == 0:
-    lista_acoes = list(dados.columns)
-elif len(lista_acoes) == 1:
-    dados = dados.rename(columns={"Close": acao_unica})
+    st.line_chart()
+    st.write("Nenhuma ação selecionada")
+if len(lista_acoes) >= 1:
+    st.line_chart(dados)
+    # calculo de performance
+    texto_performance = ""
+    if len(lista_acoes) == 0:
+        lista_acoes = list(dados.columns)
+    elif len(lista_acoes) == 1:
+        dados = dados.rename(columns={"Close": acao_unica})
+
+    carteira = [1000 for acao in lista_acoes]
+    total_inicial_carteira = sum(carteira)
 
 
-carteira = [1000 for acao in lista_acoes]
-total_inicial_carteira = sum(carteira)
+    for i, acao in enumerate(lista_acoes):
+        performance = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
+        performance = float(performance)
 
-for i, acao in enumerate(lista_acoes):
-    performance = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
-    performance = float(performance)
+        carteira[i] = carteira[i] * (1 + performance)
 
-    carteira[i] = carteira[i] * (1 + performance)
+        if  math.isnan(performance) is False:
+            if performance > 0:
+                texto_performance = texto_performance + f"  \n{acao}: :green[{performance:.1f}]%"
+            elif performance < 0:
+                texto_performance = texto_performance + f"  \n{acao}: :red[{performance:.1f}%]"
+            else:
+                texto_performance = texto_performance + f"  \n{acao}: {performance:.1f}%"
 
-    if  math.isnan(performance) is False:
-        if performance > 0:
-            texto_performance = texto_performance + f"  \n{acao}: :green[{performance:.1f}]%"
-        elif performance < 0:
-            texto_performance = texto_performance + f"  \n{acao}: :red[{performance:.1f}%]"
+
+        total_final_carteira = sum(carteira)
+        performance_carteira = total_final_carteira / total_inicial_carteira - 1
+        performance_carteira = float(performance_carteira)
+
+
+        if performance_carteira > 0:
+            texto_performance_carteira = f"  \nTotal para as ações selecionadas:   :green[{performance_carteira:.1f}%]"
+        elif performance_carteira < 0:
+            texto_performance_carteira = f"  \nTotal para as ações selecionadas:   :red[{performance_carteira:.1f}%]"
         else:
-            texto_performance = texto_performance + f"  \n{acao}: {performance:.1f}%"
-
-
-total_final_carteira = sum(carteira)
-performance_carteira = total_final_carteira / total_inicial_carteira - 1
-performance_carteira = float(performance_carteira)
-
-
-if performance_carteira > 0:
-    texto_performance_carteira = f"  \nTotal para as ações selecionadas:   :green[{performance_carteira:.1f}%]"
-elif performance_carteira < 0:
-    texto_performance_carteira = f"  \nTotal para as ações selecionadas:   :red[{performance_carteira:.1f}%]"
-else:
-    texto_performance_carteira = f"  \nTotal para as ações selecionadas:   {performance_carteira:.1f}%"
+            texto_performance_carteira = f"  \nTotal para as ações selecionadas:   {performance_carteira:.1f}%"
 
 
 
-st.write(f"""
-### Performance da Carteira
-{texto_performance_carteira}
-  \nObs: podem ocorrer erros por falhas na base de dados. Correções em andamento.   
+    st.write(f"""
+    ### Performance da Carteira
+    {texto_performance_carteira}
+    \nObs: podem ocorrer erros por falhas na base de dados. Correções em andamento.   
+    """)
+    st.write(f"""
+    #### Performance para ações e período selecionado.
+    
+    {texto_performance}
 
-### Performance dos Ativos
-Performance para ações e período selecionado.
-{texto_performance}
 
-
-""")
+    """)
